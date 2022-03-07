970ddd18eb11e9234aa96981db75f94d59ddb1df
Alex Oborin <cudlove.1@gmail.com>
2022-03-07 14:11:19 +0500

    Java IO

--- src/main/java/com/alex/io/Writer.java
+++ src/main/java/com/alex/io/Writer.java
@@ -14,6 +14,7 @@ public class Writer {
         // метод запускающий и объеиняющий все 4 класса
         inputStream();
         readObject();
+
     }
 }
 
--- src/main/java/com/alex/io/WriterController.java
+++ src/main/java/com/alex/io/WriterController.java
@@ -7,28 +7,17 @@ import static com.alex.io.Student.*;
 
 public class WriterController {
     //controller - обработка запросов от пользователя
-
     public static void inputStream() {
         WriterRepository.studentList.add(student1);
         WriterRepository.studentList.add(student2);
         WriterRepository.studentList.add(student3);
         WriterRepository.studentList.add(student4);
         WriterRepository.studentList.add(student5);
-
-
         try {
             FileOutputStream fos = new FileOutputStream("C:/Users/cudlo/Desktop/2.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeInt(WriterRepository.studentList.size());
-
-            for (Object students: WriterRepository.studentList) {
-                oos.writeObject(students);
-            }
-/*            oos.writeObject(student1);
-            oos.writeObject(student2);
-            oos.writeObject(student3);
-            oos.writeObject(student4);
-            oos.writeObject(student5);*/
+            oos.writeObject(WriterRepository.studentList);
             System.out.println("Данные успешно записаны в файл");
             oos.close();
         } catch (FileNotFoundException e) {
@@ -37,7 +26,6 @@ public class WriterController {
         } catch (IOException e) {
             e.printStackTrace();
         }//узнать что за исключение и перевести на человечий
-
     }
 
     public static void readObject() {
@@ -45,27 +33,14 @@ public class WriterController {
             FileInputStream fis = new FileInputStream("C:/Users/cudlo/Desktop/2.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            int countStudents = ois.readInt();
-            Student[] students1 = new Student[countStudents];
-
-            for (int i = 0; i < countStudents; i++) {
-                students1[i] = (Student) ois.readObject();
+            int studentsCount = ois.readInt();
+            Student students1 = new Student(WriterRepository.studentList.size());
+            for (int i = 0; i < ; i++) {
+                
             }
-            System.out.println(Arrays.toString(students1));
 
-
-/*            Student student1 = (Student) ois.readObject();
-            Student student2 = (Student) ois.readObject();
-            Student student3 = (Student) ois.readObject();
-            Student student4 = (Student) ois.readObject();
-            Student student5 = (Student) ois.readObject();*/
-
-/*            System.out.println(student1);
-            System.out.println(student2);
-            System.out.println(student3);
-            System.out.println(student4);
-            System.out.println(student5);*/
-
+/*            Student students = (Student) ois.readObject();
+            System.out.println(students);*/
             ois.close();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
